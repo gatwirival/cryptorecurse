9fb4b6ffd9507741d6127e013ab00667e469e868
valentineG <61587290+gatwirival@users.noreply.github.com>
2024-04-23 15:30:34 +0300

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Button, Menu, Typography, Avatar } from 'antd';
+import { Button, Menu, Typography } from 'antd';
 import { Link } from 'react-router-dom';
 import { HomeOutlined, BulbOutlined, FundOutlined, MenuOutlined } from '@ant-design/icons';
 
@@ -28,24 +28,23 @@ const Navbar = () => {
   }, [screenSize]);
 
   return (
-    <div className="nav-container">
-      <div className="logo-container">
-        <Avatar src={icon} size="large" />
-        <Typography.Title level={2} className="logo"><Link to="/">Cryptorecurse</Link></Typography.Title>
+    <div className="nav-container" style={{ padding: '10px 20px' }}>
+      <div className="logo-container" style={{ display: 'flex', alignItems: 'center', marginBottom: '10px' }}>
+        <Typography.Title level={3} className="logo" style={{ margin: 0 }}><Link to="/">Cryptorecurse</Link></Typography.Title>
         <Button className="menu-control-container" onClick={() => setActiveMenu(!activeMenu)}><MenuOutlined /></Button>
       </div>
       {activeMenu && (
-      <Menu theme="">
-        <Menu.Item icon={<HomeOutlined />}>
-          <Link to="/" style={{ color: 'black' }}>Home</Link>
-        </Menu.Item>
-        <Menu.Item icon={<FundOutlined />}>
-          <Link to="/cryptocurrencies" style={{ color: 'black' }}>Cryptocurrencies</Link>
-        </Menu.Item>
-        <Menu.Item icon={<BulbOutlined />}>
-          <Link to="/news" style={{ color: 'black' }}>News</Link>
-        </Menu.Item>
-      </Menu>
+        <Menu theme="" style={{ width: '100%' }}>
+          <Menu.Item icon={<HomeOutlined />}>
+            <Link to="/" style={{ color: 'black' }}>Home</Link>
+          </Menu.Item>
+          <Menu.Item icon={<FundOutlined />}>
+            <Link to="/cryptocurrencies" style={{ color: 'black' }}>Cryptocurrencies</Link>
+          </Menu.Item>
+          <Menu.Item icon={<BulbOutlined />}>
+            <Link to="/news" style={{ color: 'black' }}>News</Link>
+          </Menu.Item>
+        </Menu>
       )}
     </div>
   );
